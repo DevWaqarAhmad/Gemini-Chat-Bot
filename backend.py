b59a12573282c81fa61f6803ba6312326da8cfb9
Waqar Ahmad <devwaqarahmad@gmail.com>
2025-03-04 16:47:01 +0500

    add file

--- backend.py
+++ backend.py
@@ -3,7 +3,9 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key="AIzaSyBL-AxG9VvXh36fN1HidspNonA11DX4jgI")
+#genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
 
 # Create the model
 generation_config = {
