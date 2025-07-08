d5ce7513cf9252e2d0d19f31b22792c072f597ea
Waqar Ahmad <devwaqarahmad@gmail.com>
2025-07-08 13:54:53 +0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -18,7 +18,7 @@ CORS(app)
 # load_dotenv()
 
 # API Key for Google Generative AI
-API_KEY = "AIzaSyDuBTqfrpAjTcJFh4kYVtIVAQvlEKMPyco"
+API_KEY = "AIzaSyBIO9WyVWUZHxS07chZ2E-LOXQ0YOqB_yU"
 
 if not API_KEY:
     raise ValueError("ERROR: GEMINI_API_KEY is missing! Set it in your environment.")
